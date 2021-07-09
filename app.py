d05a7ee955bca67ddf25fb80fbeba9afc785e673
Khaled El Hamzi <contact@elhamzi.me>
2021-07-09 22:41:16 +0200

    separation principle

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ print(workbook.sheetnames)
 feuil = workbook["Feuil"]
 sheet = workbook["Sheet"]
 
-print("rows:", sheet.max_row)
-print("columns:", sheet.max_column)
+# print("rows:", sheet.max_row)
+# print("columns:", sheet.max_column)
 
 
 # access a cell
@@ -16,9 +16,9 @@ print("columns:", sheet.max_column)
 # cells = sheet["a1:c4"] ## use coordinates
 # cells = sheet[1] ## return cells in the first row
 # add a row in the the end of the sheet
-feuil.append([10003, 4,  '$20.99'])
+# feuil.append([10003, 4,  '$20.99'])
 # sheet.insert_rows(0,1) ## insert a row in the given index
-sheet.delete_rows(13, 1)
+# sheet.delete_rows(13, 1)
 # print(cells)
 # cell = sheet.cell(row=1, column=1)
 # print(cell.value)
@@ -31,4 +31,13 @@ sheet.delete_rows(13, 1)
 #     for column in range(1, sheet.max_column + 1):
 #         cell = sheet.cell(row, column)
 #         print(cell.value)
+
+workbook.create_sheet("Ark")
+
+# ! command Query separation principle
+for row in range(1, 10):
+    cell = sheet.cell(row, 3)
+    print(cell.value)
+
+
 workbook.save("_orders.xlsx")
